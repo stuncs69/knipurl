5ac57caa55f5cfa0b5901fd0686b48ecb9f480ab
NikkieDev <dev.nikkie@gmail.com>
2023-12-27 22:43:02 +0100

    bugfix

--- src/api/online.php
+++ src/api/online.php
@@ -3,14 +3,14 @@ $data_stream = file_get_contents("php://input");
 $data_parsed = json_decode($data_stream);
 
 $curl_handle = curl_init($data_parsed->url);
+curl_setopt($curl_handle, CURLOPT_NOBODY, true);
 curl_exec($curl_handle);
 
-$return = curl_getinfo($curl_handle);
-$http = $return["http_code"];
+$http = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
 
 $r_body = array("httpc"=>$http, "success"=>NULL);
 
-if ($http > 200 && $http < 400 && $http != 0)
+if ($http >= 200 && $http < 400 && $http != 0)
   $r_body["success"] = 0;
 else
   $r_body["success"] = -1;
