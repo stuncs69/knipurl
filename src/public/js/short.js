1b1e2ff44f80657ec472dcc6564ec25fe9cb5742
NikkieDev <dev.nikkie@gmail.com>
2023-12-27 23:22:24 +0100

    Some bugfixes
    Fixed https:// & http:// issue prefixing the URL. Replaced mobile button with text due to clipboard limitations.

--- src/public/js/short.js
+++ src/public/js/short.js
@@ -18,16 +18,26 @@ async function siteExists() {
 
 async function sendData() {
   submitBtn.disabled = "disabled";
+  if (inputField.value.startsWith("http://")) inputField.value = inputField.value.slice(7);
+  else if (inputField.value.startsWith("https://")) inputField.value = inputField.value.slice(8);
+
   if (await siteExists() == true) {
     await fetch("../api/shorten.php", { method: "POST", body: JSON.stringify({ url: inputField.value, name: inputField.value.split(".")[0] }) })
     .then(r => r.json()).then(r => {
       const ELEM = document.createElement("div");
       ELEM.id = "short-data-actual";
       ELEM.classList = "text-white d-flex flex-column gap-2";
-      ELEM.innerHTML = `
+      if (window.innerWidth > 768) {
+        ELEM.innerHTML = `
         <button class='btn bg-primary text-white' id='copy-btn'>Copy to clipboard</button>
         <a href='${r.shortAnal}'><button class='btn bg-primary text-white'>Analytics</button></a>
       `;
+      } else {
+        ELEM.innerHTML = `
+        <a href='${r.shortUrl}'<button class='btn' style="background:#0A0C10; color: white;">${r.shortUrl}</button>
+        <a href='${r.shortAnal}'><button class='btn bg-primary text-white'>Analytics</button></a>
+      `;
+      }
 
       const SHORT_DATA = document.querySelector("#short-data");
       if (SHORT_DATA.innerHTML.length > 0) SHORT_DATA.innerHTML = "";
@@ -35,8 +45,15 @@ async function sendData() {
       SHORT_DATA.classList.add("p-2")
       SHORT_DATA.innerHTML += "<h5>Short link:</h5>"
       SHORT_DATA.appendChild(ELEM);
-
-      document.querySelector("#copy-btn").addEventListener("click", e=> {navigator.clipboard.writeText(r.shortUrl); alert("Copied!")} );
+      
+      if (window.innerWidth > 768) {
+        document.querySelector("#copy-btn").addEventListener("click", e=> {
+          navigator.clipboard
+          .writeText(r.shortUrl)
+          .then(() => alert("Copied!"))
+          .catch(() => alert("Something went wrong, please try again later."));
+        });
+      }
     }).catch(e => {
       alert("Couldn't shorten link, try again later.");
       console.error(e)
