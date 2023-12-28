537780cf6328e03cb0fab24923b8bb773a0cc30c
NikkieDev <dev.nikkie@gmail.com>
2023-12-28 17:29:37 +0100

    Release

--- src/public/a/index.php
+++ src/public/a/index.php
@@ -15,18 +15,16 @@ else {
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1583080053162182" crossorigin="anonymous"></script>
-  <meta name="google-adsense-account" content="ca-pub-1583080053162182">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <title><?php echo $data_parsed["code"]; ?></title>
 </head>
-<body style="width: 240px">
+<body style="max-width: 768px;">
   <div class="d-flex flex-column bg-secondary p-2 gap-2 text-white">
     <div class="d-flex flex-row gap-2 bg-primary rounded p-1">
       <p>Url: </p>
-      <span><?php echo $data_parsed["long_url"]; ?></span>
+      <span class="d-flex flex-wrap"><?php echo $data_parsed["long_url"]; ?></span>
     </div>
     <div class="d-flex flex-row gap-2 bg-primary rounded p-1">
       <p>Code: </p>
@@ -37,5 +35,6 @@ else {
       <span><?php echo $data_parsed["views"]; ?></span>
     </div>
   </div>
+  <script type="text/javascript"> var infolinks_pid = 3411827; var infolinks_wsid = 0; </script> <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
 </body>
 </html>
--- src/public/url/index.php
+++ src/public/url/index.php
@@ -28,3 +28,4 @@ else {
   }
 }
 ?>
+<script type="text/javascript"> var infolinks_pid = 3411827; var infolinks_wsid = 0; </script> <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
